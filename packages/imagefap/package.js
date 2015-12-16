55d243925f2e81fbf073eadefc0dcd4db24c224d
nxpeacock <nguyenxuancong@gmail.com>
2015-12-17 00:22:51 +0700

    remove depen osmosis

--- packages/imagefap/package.js
+++ packages/imagefap/package.js
@@ -13,7 +13,7 @@ Package.describe({
 Package.onUse(function (api) {
     api.versionsFrom('1.2.1');
     api.use('ecmascript');
-    api.use(['gnocxn:osmosis'],['server']);
+    api.use(['me:x-ray'],['server']);
     api.addFiles('imagefap.js','server');
 });
 
