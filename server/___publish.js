f95f9f08d4850b774dc51eebb1426b70ac4b069d
nxpeacock <nguyenxuancong@gmail.com>
2015-12-21 00:07:43 +0700

    1

--- server/___publish.js
+++ server/___publish.js
@@ -1,5 +1,5 @@
 Meteor.publish('getAlbums',function(param,limit){
-    return Albums.find(param || {},{limit : limit || 50});
+    return Albums.find(param || {},{limit : limit || 50, sort : {updatedAt : -1}});
 });
 
 
